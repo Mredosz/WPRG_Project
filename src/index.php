e0d01596d9140e67dbae90a0019d20948d03b9ab
MR <mredosz4@gmail.com>
2023-04-23 17:00:17 +0200

    Nav bar

---
+++ src/index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <!--    Website Title-->
+    <title>Shrek's Restaurant</title>
+    <!--    Custom CSS-->
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <!--    Website icon-->
+    <link rel="icon" href="/image/icon.ico">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css">
+    <!-- Bootstrap JavaScript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
+    <!--    Icons-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+</head>
+<body id="myPage" data-spy="scroll" data-target=".navbar" data-offset="60">
+<!--Navbar-->
+<nav class="navbar navbar-expand-md bg-dark navbar-dark fixed-top">
+    <a class="navbar-brand" href="#">Shrek's Restaurant</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collaosibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="collaosibleNavbar"></div>
+<!--    #about - go to another section in this file-->
+    <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+            <a class="nav-item" href="#about">About</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-item" href="#menu">Menu</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-item" href="#service">Service</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-item" href="#contact">Contact</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-item" href="singUp.php">Sing up</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-item" href="logIn.php">Log in</a>
+        </li>
+    </ul>
+</nav>
+<!--    Header-->
+<div class="jumbotron">
+    <div class="container-fluid">
+        <div class="header-content-inner">
+            <h1>Welcome to Shrek's Restaurant</h1>
+            <h3>Text about restaurant</h3>
+        </div>
+    </div>
+</div>
+
+
+</body>
+</html>
