515a42f395d35d958819ace69925fd06f43d2f89
MR <mredosz4@gmail.com>
2023-05-21 16:15:59 +0200

    new constants

--- src/constants.php
+++ src/constants.php
@@ -1,6 +1,7 @@
 <?php
 const MHTML = 'src/main_website/html_file';
 const UHTML = 'src/users_website/html_file';
+const MPHP = 'src/main_website/php_file';
 const IMAGE_ABOUT = '../../../image';
 
 
