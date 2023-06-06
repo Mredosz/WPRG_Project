6a20cb9077ade3c32cdf917f7e781d55cf8c345c
MR <mredosz4@gmail.com>
2023-06-06 19:21:00 +0200

    Dashboard

--- src/admin_website/php_file/dashboard.php
+++ src/admin_website/php_file/dashboard.php
@@ -2,32 +2,44 @@
     <h1 class="text-center">Dashboard</h1>
     <div class="row">
         <div class="col">
-            <h2 class="text-center">Orders Info</h2>
+            <div class="card" style="width: 18rem;">
+                <img src="../../../image/category.png" class="card-img-top" alt="..." style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">Category</h5>
+                    <p class="card-text">Go there to manage categories</p>
+                    <a href="category.php" class="btn btn-outline-dark">Go to Category.</a>
+                </div>
+            </div>
         </div>
         <div class="col">
-            <h2 class="text-center">Most popular dishes</h2>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec dolor in est euismod lobortis. Fusce
-                nec laoreet massa. Ut molestie purus ex, ut congue purus accumsan quis. Donec vel arcu sed lacus commodo
-                consequat. Maecenas tempor tortor sed augue blandit fermentum. Suspendisse at turpis non arcu iaculis
-                tristique. Aliquam vestibulum, elit quis aliquet pellentesque, nibh dolor sodales justo, sit amet
-                lobortis ex ante sed mauris. Nunc convallis eros risus, dignissim dignissim felis sodales at. Donec
-                sodales nec mauris ac eleifend. Pellentesque porta sapien eget nisl interdum, a egestas justo
-                scelerisque. Duis erat sapien, ullamcorper vel tempor sed, varius sit amet dolor. Pellentesque habitant
-                morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+            <div class="card" style="width: 18rem;">
+                <img src="../../../image/menu.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Menu</h5>
+                    <p class="card-text">Go there to manage menu.</p>
+                    <a href="menu.php" class="btn btn-outline-dark">Go to Menu</a>
+                </div>
+            </div>
         </div>
         <div class="col">
-            <p>Donec maximus enim sed nunc sollicitudin, et scelerisque quam ullamcorper. Mauris nec justo et est
-                ullamcorper accumsan. Fusce faucibus est vitae gravida finibus. Morbi molestie augue ac ligula feugiat,
-                et volutpat orci mattis. Nulla placerat dui nec libero rhoncus, ut sodales nulla sollicitudin. Duis
-                consequat risus facilisis nisl ultrices, ac ullamcorper massa egestas. Aenean luctus ex sed fermentum
-                condimentum. In libero dui, posuere id efficitur nec, suscipit sit amet sapien. In hac habitasse platea
-                dictumst. Vestibulum ullamcorper lacinia rhoncus. Maecenas nec magna sed ligula pretium venenatis ut ac
-                nunc. Suspendisse pulvinar suscipit blandit. Fusce sagittis, neque at vestibulum maximus, ante erat
-                laoreet risus, fermentum mollis tortor ipsum eget lectus. Cras eu leo metus.</p>
+            <div class="card" style="width: 18rem;">
+                <img src="../../../image/user.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Users</h5>
+                    <p class="card-text">Go there to manage users.</p>
+                    <a href="users.php" class="btn btn-outline-dark">Go to Users</a>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card" style="width: 18rem;">
+                <img src="../../../image/order.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Orders</h5>
+                    <p class="card-text">Go there to see all orders.</p>
+                    <a href="orders.php" class="btn btn-outline-dark">Go to Orders</a>
+                </div>
+            </div>
         </div>
     </div>
 </div>
